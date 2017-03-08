347c2c6e35a84d99357f4f8c392293f4c3309f4f
LiamAkkerman <liam.akkerman@gmail.com>
2017-03-07 22:59:15 -0800

    add 3rd segment

--- path.m
+++ path.m
@@ -13,6 +13,7 @@ syms sx_brach sy_brach
 hmax = 889; %starting hieght in mm
 segs = 7;   %number of line segments
 mmininch = 25.4; %how many mm in an inch
+balldia = 15; %diameter of marble, 15 is a guess
 
 %initialize empty vectors for hieghts, starts, and ends
 h = NaN(segs);
@@ -29,13 +30,12 @@ sx_brach = 0.5*h_brach*(t-sin(t));
 sy_brach = -0.5*h_brach*(1-cos(t));
 
 
-
-%define first section hieght
-h(1) = 3*mmininch;        
+     
 
 
 i = 1; %let's try this way, still need to iterate
 %substitute a values into brach functions
+h(i) = 75;
 sx(i) = subs(sx_brach, h_brach, h(1));    %define parametic equations for 1st Brachistochrone curve
 sy(i) = subs(sy_brach, h_brach, h(1)) + hmax;
 deriv(i) = simplify(diff(sy(i))/diff(sx(i)));
@@ -43,30 +43,47 @@ deriv2(i) = simplify(diff(deriv(i)));
 theta(i) = simplify(atan(deriv(i)));
 %TODO make function for all of these
 
+%the max is made up to have a upwards slope for the sick jump
 tmin(i) = 0;
-tmax(i) = 3.14*1.25;           %max t for 1st section
+tmax(i) = 3.14*1.25;
 %TODO automate max t
 
 ezplot(sx(i),sy(i),[tmin(i),tmax(i)]) 
 hold on;
 
 i = 2;
+%linear segment, maybe we'll keep, maybe not
+%inherits perovous derivitive
 deriv(i) = subs(deriv(i-1), t, tmax(i-1));
 deriv2(i) = simplify(diff(deriv(i)));
 theta(i) = simplify(atan(deriv(i)));
 
+%parametric equation of line
 sx(i) = subs(sx(i-1), t, tmax(i-1)) + t;
 sy(i) = subs(sy(i-1), t, tmax(i-1)) + t*deriv(i);
 
+%I made the max up randomly
 tmin(i) = 0;
 tmax(i) = 10;
 
-
-
+%add it to the plot
 ezplot(sx(i),sy(i),[tmin(i),tmax(i)]) 
 
+
+i = 3;
+h(i) = 85;
+sx(i) = -subs(sx_brach, h_brach, h(i)) + 230;    %define parametic equations for 1st Brachistochrone curve
+sy(i) = subs(sy_brach, h_brach, h(i)) + 850;
+deriv(i) = 0.45;
+deriv2(i) = simplify(diff(deriv(i)));
+theta(i) = simplify(atan(deriv(i)));
+
+tmin(i) = 0;
+tmax(i) = 3.14;
+
+ezplot(sx(i),sy(i),[tmin(i),tmax(i)]) 
                         
 %for i = 1:segs
-    ezplot(sx(i),sy(i),[tmin(i),tmax(i)])     %plot sections   
+%    ezplot(sx(i),sy(i),[tmin(i),tmax(i)])     %plot sections   
 %end    
 
